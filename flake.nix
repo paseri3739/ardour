6df0c2a574d85d03af8d37900054d1b9611f442a
Masato <tael43@icloud.com>
2026-03-08 22:08:19 +0900

    rpath

--- flake.nix
+++ flake.nix
@@ -68,8 +68,16 @@
           ];
           buildInputs = libraries;
           shellHook = ''
-            # sratom-0のインクルードパス解決
+            # 既存の設定
             export NIX_CFLAGS_COMPILE="$(pkg-config --cflags sratom-0) $NIX_CFLAGS_COMPILE"
+
+            # RPATHの追加
+            # -Wl,-rpath, はリンカに実行時探索パスを追加する指示です
+            VAMP_LIB_PATH="${pkgs.vamp-plugin-sdk}/lib"
+            export LDFLAGS="-L$VAMP_LIB_PATH -Wl,-rpath,$VAMP_LIB_PATH $LDFLAGS"
+
+            # 他のライブラリも一括で対象にする場合は以下が効率的です
+            export LDFLAGS="-Wl,-rpath,${pkgs.lib.makeLibraryPath libraries} $LDFLAGS"
           '';
         };
       }
